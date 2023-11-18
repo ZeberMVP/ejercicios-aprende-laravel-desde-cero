0003d65eb9409d012dd920097bad85941ad711f0
zebermvp <rubeen722@gmail.com>
2023-11-18 19:25:52 +0100

    solución 2

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,45 @@ Route::delete('/ejercicio1', function () {
 Route::patch('/ejercicio1', function () {
     return "PATCH OK";
 });
+
+ROUTE::post('/ejercicio2/a', function (Request $request) {
+    return Response::json([
+        'name' => $request->name,
+        'description' => $request->description,
+        'price' => $request->price,
+    ], 200);
+});
+
+ROUTE::post('/ejercicio2/b', function (Request $request) {
+
+    if ($request->price < 0) {
+        return Response::json([
+            "message" => "Price can't be less than 0"
+        ], 422);
+    }
+
+    return Response::json([
+        'name' => $request->name,
+        'description' => $request->description,
+        'price' => $request->price,
+    ], 200);
+});
+
+ROUTE::post('/ejercicio2/c/{discount?}', function (Request $request) {
+
+    $discount = 0;
+    if ($request->query('discount') == "SAVE5") {
+        $discount = 5;
+    } else if ($request->query('discount') == "SAVE10") {
+        $discount = 10;
+    } else if ($request->query('discount') == "SAVE15") {
+        $discount = 15;
+    }
+
+    return Response::json([
+        'name' => $request->name,
+        'description' => $request->description,
+        'price' => $request->price - ($request->price * $discount / 100),
+        'discount' => $discount,
+    ], 200);
+});
